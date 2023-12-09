dd42c305f3c550bdfefe11da4ff6efe39d59568a
Cristian <cristian.jimenezsa@hotmail.com>
2023-12-09 00:44:06 -0500

    error handling

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -1,4 +1,8 @@
 const errorHandler = (err) => {
+  if (err.code === 'ERR_NETWORK') {
+    return `${err.message}, try again later`;
+  }
+
   const { error } = err.response.data;
   return error;
 };
